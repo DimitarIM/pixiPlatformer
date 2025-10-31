19df4b4c7ebb24c433b5fd392e38aceafa32bccf
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-10-31 15:50:52 +0100

    initial commit

---
+++ src/scripts/game/Config.js
@@ -0,0 +1,54 @@
+import { Tools } from "../system/Tools";
+import { Game } from "./Game";
+
+export const Config = {
+    bgSpeed: 2,
+    score: {
+        x: 10,
+        y: 10,
+        anchor: 0,
+        style: {
+            fontFamily: "Verdana",
+            fontWeight: "bold",
+            fontSize: 44,
+            fill: ["#FF7F50"]
+        }
+    },
+    platforms: {
+        moveSpeed: -1.5,
+        ranges: {
+            rows: {
+                min: 2,
+                max: 6
+            },
+            cols: {
+                min: 3,
+                max: 9,
+            },
+            offset: {
+                min: 60,
+                max: 200,
+            }
+        }
+    },
+    hero: {
+        jumpSpeed: 12,
+        maxJumps: 2,
+        position: {
+            x: 350,
+            y: 590,
+        }
+    },
+    diamonds: {
+        chance: 0.4,
+        offset: {
+            min: 100,
+            max: 200
+        }
+    },
+    loader: Tools.massiveRequire(require["context"]('./../../sprites/', true, /\.(mp3|png|jpe?g)$/)),
+    startScene: Game,
+    scenes: {
+        "Game": Game
+    },
+}
---
+++ src/scripts/system/App.js
@@ -0,0 +1,38 @@
+import * as PIXI from "pixi.js";
+import * as Matter from "matter-js";
+import { ScenesManager } from "./SceneManager";
+import { Loader } from "./Loader";
+
+class Application {
+    run(config) {
+        this.config = config;
+        this.app = new PIXI.Application({ resizeTo: window });
+        document.body.appendChild(this.app.view);
+
+        this.loader = new Loader(this.app.loader, this.config);
+        this.loader.preload().then((() => this.start()));
+
+        this.scenes = new ScenesManager();
+        this.app.stage.interactive = true;
+        this.app.stage.addChild(this.scenes.container);
+
+        this.createPhysics();
+    }
+    start() {
+        this.scenes.start("Game");
+    }
+    res(key) {
+        return this.loader.resources[key].texture;
+    }
+    sprite(key) {
+        return new PIXI.Sprite(this.res(key));
+    }
+
+    createPhysics(){
+        this.physics = Matter.Engine.create();
+        const runner = Matter.Runner.create();
+        Matter.Runner.run(runner, this.physics);
+    }
+}
+
+export const App = new Application();
